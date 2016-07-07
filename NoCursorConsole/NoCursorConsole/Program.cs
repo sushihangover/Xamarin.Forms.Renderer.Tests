0a392e56f0bef2d5fd0cdaafdd60677292460ce9
SushiHangover <sushihangover@outlook.com>
2016-07-07 14:20:24 -0700

    How to hide the cursor in a C# console application on Linux?
    http://stackoverflow.com/questions/38251164/how-to-hide-the-cursor-in-a-c-sharp-console-application-on-linux

---
+++ NoCursorConsole/NoCursorConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace NoCursorConsole
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Process.Start("tput", "civis -- invisible");
+			Console.WriteLine("Hello World!");
+			Console.Read(); // no cursor
+			Process.Start("tput", "cnorm -- normal");
+			Console.Read(); // cursor
+		}
+	}
+}
