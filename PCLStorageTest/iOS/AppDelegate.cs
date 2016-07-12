e6753458b4f764d7a019c67f7883466d936d87b6
SushiHangover <sushihangover@outlook.com>
2016-07-11 18:57:00 -0700

    PCLStorage - What platform directory is mapped
    http://stackoverflow.com/questions/38304907/xamarin-pclstorage-package-what-is-the-root-folder-this-pclstorage-reference-to

---
+++ PCLStorageTest/iOS/AppDelegate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace PCLStorageTest.iOS
+{
+	[Register("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
+			global::Xamarin.Forms.Forms.Init();
+
+			LoadApplication(new App());
+
+			var documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			Console.WriteLine(documents);
+
+			return base.FinishedLaunching(app, options);
+		}
+	}
+}
+
