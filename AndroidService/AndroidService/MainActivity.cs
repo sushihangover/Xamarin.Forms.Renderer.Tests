85566169e1fdd770896f1549e142a20a8870dcf0
SushiHangover <sushihangover@outlook.com>
2016-08-16 12:11:16 -0700

    Android App starting a Service in another App / Explict Intent
    http://stackoverflow.com/questions/38971274/retrieving-the-component-name-of-a-service-defined-in-another-xamarin-applicatio/38973871#38973871

---
+++ AndroidService/AndroidService/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using System;
+using Android.Util;
+
+namespace AndroidService
+{
+	[Service(Name="com.sushihangover.androidservice.MyMostAmazingService", Exported = true)]
+	[IntentFilter(new String[] { "myservice" }, Categories = new[] { Intent.CategoryDefault })]
+	public class PsiServiceHost : Service
+	{
+		public override IBinder OnBind(Intent intent)
+		{
+			return null;
+		}
+		public override StartCommandResult OnStartCommand(Intent intent, StartCommandFlags flags, int startId)
+		{
+			Log.Debug("SO", "MyMostAmazingService Has Been Started");
+			return base.OnStartCommand(intent, flags, startId);
+		}
+	}
+
+	[Activity(Label = "AndroidService", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		bool started;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+			button.Click += delegate {
+				if (started)
+					StartService(new Intent("my.very.special.service"));
+				else
+					StopService(new Intent("my.very.special.service"));
+				started = !started;
+			};
+		}
+	}
+}
+
---
+++ AndroidService/AndroidServiceStarter/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace AndroidServiceStarter
+{
+	[Activity(Label = "AndroidServiceStarter", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		bool started = false;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+			button.Click += delegate
+			{
+				var intent = new Intent();
+				intent.SetClassName("com.sushihangover.androidservice", "com.sushihangover.androidservice.MyMostAmazingService");
+				if (started)
+					StartService(intent);
+				else
+					StopService(new Intent(intent));
+				started = !started;
+			};
+		}
+	}
+}
+
